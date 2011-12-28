4fbd29b4d3f3695a710154e0037a3929f8132c8d
Jaime Cruz <jhcruz@zoho.com>
2011-12-28 10:42:20 -0600

    imagenes y links de las mismas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  #Return a title on a per-page basis
+  def title
+    base_title= "Evaluacion de los ingenieros de Campo | CNOC"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+    image_tag("cnoc.png", :alt => "Evaluacion IDC", :class => "round")
+  end
 end
